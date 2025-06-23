eaf561db6fff741e629aa01c1431993935a3ecf1
Dmitry Suraho <54984286+dmitrysuraho@users.noreply.github.com>
2025-06-23 20:10:51 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const crypto = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const KEYS_FILE = 'keys.json';
 
